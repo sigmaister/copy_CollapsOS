41cd8086d46d222660ec2c992a264203185070d3
Virgil Dupras <hsoft@hardcoded.net>
2020-03-27 21:15:16 -0400

    forth: don't use "(entry)" in dummy.fs
    Because icore.fs defines it in a "not-for-runtime" manner, dummy.fs
    executing it had ill effects. We use a workaround instead.

--- forth/dummy.fs
+++ forth/dummy.fs
@@ -1,7 +1,9 @@
 ( When building a compiled dict, always include this unit at
   the end of it so that Forth knows how to hook LATEST into
-  it )
-(entry) _
+  it. We don't use the word "(entry)" to avoid messing up
+  with icore setup. )
+CREATE _
+H@ 2 - HERE !
 
 ( After each dummy word like this, we poke IO port 2 with our
   current HERE value. The staging executable needs it to know
